ffe031f31ad5999736a52e74d81386cc81464c44
int <125262137+StructuredObjects@users.noreply.github.com>
2023-07-12 05:28:58 -0700

    gg

--- src/protocols/botnets.py
+++ src/protocols/botnets.py
@@ -9,6 +9,11 @@ class QBOT_M():
     methods:        str;
     method_usage:   str;
     max_time:       int;
-    
+
 class BreezyQBOT():
     qbots:      list[QBOT_M];
+    def __init__(self, qbot_m: dict):
+        self.__parseMethods();
+
+    def __parseMethods(self) -> bool:
+        pass
